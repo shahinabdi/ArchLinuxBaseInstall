444d53ef74888791a797a7141b9ca02e1463e744
Shahin ABDI-Arch <shahin.abdi.pro@gmail.com>
2022-04-17 12:21:50 +0200

    locale.gen bug fixed

--- base-uefi.sh
+++ base-uefi.sh
@@ -2,7 +2,7 @@
 
 ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 hwclock --systohc
-sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' locale.gen
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=fr" >> /etc/vconsole.conf
